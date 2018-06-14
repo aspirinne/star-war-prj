bae7eb56b19b5604f562a95af75cdb955f328066
aspirinne <buranweb@gmail.com>
2018-06-14 16:01:13 +0300

    Added files and changes required for deployment to heroku -- seven

--- star_wars/settings.py
+++ star_wars/settings.py
@@ -21,10 +21,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'c_*bra7fy$p*)gu7e72fsruzrtojxv937%1%7h)da*bh9qwy(l'
+# SECRET_KEY = 'c_*bra7fy$p*)gu7e72fsruzrtojxv937%1%7h)da*bh9qwy(l'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'c_*bra7fy$p*)gu7e72fsruzrtojxv937%1%7h)da*bh9qwy(l')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+# DEBUG = True
+DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 
 ALLOWED_HOSTS = []
 
