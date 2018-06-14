41ecd59d4e3c4e30709bb85019184432a048cfc7
aspirinne <buranweb@gmail.com>
2018-06-14 17:02:47 +0300

    Last comm3

--- star_wars/settings.py
+++ star_wars/settings.py
@@ -34,14 +34,14 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
-    'selection_committee.apps.SelectionCommitteeConfig',
+    # 'selection_committee.apps.SelectionCommitteeConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # 'selection_committee',
+    'selection_committee',
 ]
 
 MIDDLEWARE = [
@@ -151,4 +151,4 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
