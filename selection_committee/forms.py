224cab270c0e63f11ebde455fdf37de93bdd9bfd
aspirinne <buranweb@gmail.com>
2018-06-14 23:22:21 +0300

    Last comm8

--- selection_committee/forms.py
+++ selection_committee/forms.py
@@ -1,11 +1,14 @@
 from django import forms
-from .models import Youngling, Jedi
+from .models import Youngling
 
 
 class YounglingForm(forms.ModelForm):
     class Meta:
         model = Youngling
         exclude = ['teacher']
+        widgets = {
+            'birthday': forms.DateInput(format('%Y/%m/%d'), attrs={'placeholder': 'Электронный адрес',}),
+        }
 
 
 # class JediChooseForm(forms.Form)
