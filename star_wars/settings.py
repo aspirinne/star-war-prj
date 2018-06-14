e7984218710358d9e772a67683047323fe2ded74
aspirinne <buranweb@gmail.com>
2018-06-14 17:55:07 +0300

    Last comm7

--- star_wars/settings.py
+++ star_wars/settings.py
@@ -135,8 +135,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = os.environ['buranweb@gmail.com']
-EMAIL_HOST_PASSWORD = os.environ['Kbyljy13']
+EMAIL_HOST_USER = 'buranweb@gmail.com'
+EMAIL_HOST_PASSWORD = 'Kbyljy13'
 DEFAULT_FROM_EMAIL = 'Rafael'
 DEFAULT_TO_EMAIL = 'folko93@mail.ru'
 
