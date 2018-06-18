44764b810d9298de8a418d54a9966ef61ea1c7d2
aspirinne <buranweb@gmail.com>
2018-06-18 15:02:06 +0300

    Comm1

--- selection_committee/models.py
+++ selection_committee/models.py
@@ -1,16 +1,4 @@
-# from typing import Any, Union
-
 from django.db import models
-# from django.db.models.aggregates import Count
-# from random import randint
-
-
-# Create your models here.
-# class RandomManager(models.Manager):
-#     def random(self):
-#         count = self.aggregate(count=Count('id'))['count']
-#         random_index = randint(0, count - 1)
-#         return self.get(id=random_index)
 
 
 class Youngling(models.Model):
